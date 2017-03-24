680229c4dc7e4e9d3ee695719342c14c8565950f
Daniel Ho <daniel.ho@berkeley.edu>
2017-03-23 21:05:10 -0700

    Made working unittests

--- ENGIN7/pycharm_e7/Lab2/lab2_ag.py
+++ ENGIN7/pycharm_e7/Lab2/lab2_ag.py
@@ -1,6 +1,5 @@
 import numpy as np
 import unittest
-import lab2
 
 
 def assertTuple(self, t1, t2, err=""):
@@ -12,6 +11,8 @@ def assertTuple(self, t1, t2, err=""):
         else:
             self.assertEquals(v1, v2, err)
 
+my_sin_approx = None
+
 
 class TestQ1(unittest.TestCase):
     def test_q1(self):
@@ -19,12 +20,19 @@ class TestQ1(unittest.TestCase):
         ans2 = (-0.9589242746631385, -5.2926587301587276, -4.3337344554955894, 4.5193708929915157)
         ans3 = (0.9589242746631385, 5.2926587301587276, 4.3337344554955894, 4.5193708929915157)
         ans4 = (0.0, 0.0, 0.0, np.nan)
-        assertTuple(self, lab2.my_sin_approx(1), ans1)
-        assertTuple(self, lab2.my_sin_approx(5), ans2)
-        assertTuple(self, lab2.my_sin_approx(-5), ans3)
-        assertTuple(self, lab2.my_sin_approx(0), ans4)
+        assertTuple(self, my_sin_approx(1), ans1)
+        assertTuple(self, my_sin_approx(5), ans2)
+        assertTuple(self, my_sin_approx(-5), ans3)
+        assertTuple(self, my_sin_approx(0), ans4)
         print('Question 1 Passed!')
 
 
+def run_q1(f1):
+    global my_sin_approx
+    my_sin_approx = f1
+    q1 = unittest.TestLoader().loadTestsFromTestCase(TestQ1)
+    unittest.TextTestRunner().run(q1)
+
+
 if __name__ == '__main__':
-    unittest.main()
+    print("loaded autograder!")
