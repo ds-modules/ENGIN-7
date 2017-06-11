8db686fd8f2c3f9bda620cb37b5627d2e1ccea13
Daniel Ho <daniel.ho@berkeley.edu>
2017-06-11 10:46:10 -0700

    Finished raw solutions for first three labs

--- ENGIN7/pycharm_e7/2016/Lab1/lab1.py
+++ ENGIN7/pycharm_e7/2016/Lab1/lab1.py
@@ -1,17 +1,55 @@
 import math
 import numpy as np
+import cmath
+import scipy.io as sio
+import matplotlib.pyplot as plt
 
 def q1():
+    """
+    doctest here
+    """
     a, b, c, x = 3., -6., 4., 2.
-    E1 = math.sqrt(a*a + b*b + c*c)
-    E2 = (-b - np.sqrt(b*b - 4*a*c)) / 2*a
-    E3 = math.log(3*x - a)
-    E4 = math.log(3*abs(b) + c / 5, 10)
-    E5 = math.pow(a*x + a*b/c, 1./3)
-    E6 = (x*x + 1) / ((a*x-1) * abs(b-math.e**x))
+    E1 = math.sqrt(a*a+b*b+c*c)
+    E2 = (-b-cmath.sqrt(b*b-4*a*c))/(2*a)
+    E3 = math.log(3*x-a)
+    E4 = math.log(3*abs(b)+c/5,10)
+    E5 = math.pow(a*x+a*b/c,1./3)
+    E6 = (x*x+1)/((a*x-1)*abs(b-math.exp(x)))
     E7 = math.cos(math.sqrt(a)/3*math.pi)**2 + \
          math.cos((math.sqrt(a)/3*math.pi)**2)
+    E8 = cmath.exp(cmath.pi*cmath.sqrt(-1))
+    E9 = math.acos(math.cos(x))
+    E10 = (a+2*c)/(math.sin((b+2*c)/math.sqrt(a**2+b**2+c**2)))
 
-    return (E1,E2,E3,E4,E5,E6,E7)
+    return E1,E2,E3,E4,E5,E6,E7,E8,E9,E10
 
-q1()
+def q2():
+    # Q2A
+    year = list(range(2007,2016))
+    wins, losses = [7,9,8,5,7,3,1,5,8], [6,4,2,7,6,9,11,7,5]
+    # print(year, wins, losses)
+
+    # Q2B
+    losses[2] = 5
+
+    # Q2C
+    mat_contents = sio.loadmat('CALFB_HistoricalData.mat')
+    # print(mat_contents)
+    Y_all = np.append(np.ndarray.flatten(mat_contents['Y']), year)
+    W_all = np.append(np.ndarray.flatten(mat_contents['W']), wins)
+    L_all = np.append(np.ndarray.flatten(mat_contents['L']), losses)
+    # print(Y_all.shape, W_all.shape, L_all.shape)
+
+    # Q2D
+    fig, ax = plt.subplots()
+    plt.xlabel('Year')
+    plt.ylabel('Wins and Losses')
+    plt.title('Cal Football Data')
+    ax.set_ylim([0,15])
+    ax.plot(Y_all, W_all, label='wins', linestyle='dashed')
+    ax.plot(Y_all, L_all, label='losses', linestyle='dashed')
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles, labels)
+    plt.show()
+
+q2()
--- ENGIN7/pycharm_e7/2016/Lab2/lab2.py
+++ ENGIN7/pycharm_e7/2016/Lab2/lab2.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 
 def solve_quadratic(a, b, c):
     """
@@ -29,12 +30,48 @@ def c14_dating(time):
     """
     return math.e**(-time*0.00012097)
 
+g = 9.81
+
 def proj_time(y0, v0, theta):
     """
     >>> proj_time(0, 15, 40)
-    1.9657
+    1.9657113446071535
     """
-    b = v0 *
+    b = v0 * math.sin(np.deg2rad(theta))
+    time = -(-b - math.sqrt(b**2+2.*g*y0))/g
+    return time
 
+def proj_distance(y0, v0, theta):
+        """
+        >>> proj_distance(0, 15, 40)
+        22.587333784683665
+        """
+        time = proj_time(y0, v0, theta)
+        dist = v0 * time * math.cos(np.deg2rad(theta))
+        return dist
 
+def draw_circle(xc, yc, rc):
+    circle = plt.Circle((xc, yc), rc, fill=False)
+    fig, ax = plt.subplots()
+    ax.set_xlim((-5,5))
+    ax.set_ylim((-5,5))
+    ax.add_artist(circle)
+    plt.show()
 
+def triangulate(y2,x3,y3,r1,r2,r3):
+    yCell = (r1*r1-r2*r2+y2*y2)/(2*y2)
+    c = x3*x3 + (yCell-y3)**2 - r3*r3
+    xCell = (2*x3 - math.sqrt(4*x3*x3-4*c))/2
+    fig, ax = plt.subplots()
+    ax.set_xlim((-20, 20))
+    ax.set_ylim((-20, 20))
+    circle1 = plt.Circle((0,0), r1, fill=False)
+    circle2 = plt.Circle((0,y2), r2, fill=False)
+    circle3 = plt.Circle((x3,y3), r3, fill=False)
+    ax.plot((xCell), (yCell), '^', color = 'blue')
+    ax.add_artist(circle1)
+    ax.add_artist(circle2)
+    ax.add_artist(circle3)
+    plt.show()
+
+triangulate(3,8,1,2.24,1.41,7.07)
